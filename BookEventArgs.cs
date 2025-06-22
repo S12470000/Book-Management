683c3a4f976f1817173b9fd32ffecc1d50d78013
sunil saini <sunilsaini67193@gmail.com>
2025-06-22 07:33:41 +0530

    Add files via upload

---
+++ BookEventArgs.cs
@@ -0,0 +1,7 @@
+﻿using Book_Manag.Models;
+
+public class BookEventArgs : EventArgs
+{
+    public Book Book { get; }
+    public BookEventArgs(Book book) => Book = book;
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Book_Manag.Data;
+using Book_Manag.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString)
+);
+
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddControllers();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Enable Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
